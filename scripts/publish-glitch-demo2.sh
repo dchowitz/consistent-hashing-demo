c971df46367515bd30f1ae678089b514efb93c50
dchowitz <denny.christochowitz@gmail.com>
2021-12-31 17:50:37 +0100

    glitch publish convenience script

--- scripts/publish-glitch-demo2.sh
+++ scripts/publish-glitch-demo2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-$glitch_dir=~/Dev/glitch/dchowitz-consistent-hashing-demo2
+glitch_dir=~/Dev/glitch/dchowitz-consistent-hashing-demo2
+
+echo $glitch_dir
 
 rm -rf $glitch_dir/*.*
 cp -r ./dist/*.* $glitch_dir
